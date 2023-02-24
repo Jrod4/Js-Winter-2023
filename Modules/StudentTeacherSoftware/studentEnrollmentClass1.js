ff34cc73233268edc972a99f41c0cf625412acb6
Jeffrey Rodriguez <jeffreyrodriguez@Jeffreys-MacBook-Pro.local>
2023-02-23 22:25:50 -0500

    Class Concept Revised

--- Modules/StudentTeacherSoftware/studentEnrollmentClass1.js
+++ Modules/StudentTeacherSoftware/studentEnrollmentClass1.js
@@ -12,37 +12,66 @@
  */
 
 class StudentEnrollment1 {
+  studentInformation = {
+    sid: 0,
+    sName: "Jeff Rod",
+    sAge: 29,
+    sCourseName: "Web Development",
+    sBalance: 5000,
+    sGrade: "A",
+  };
+
+  courseInformation = [
+    {
+      cName: "Web Development",
+      cPrice: 5000,
+    },
+    {
+      cName: "SDET Course",
+      cPrice: 4000,
+    },
+    {
+      cName: "Backend Development",
+      cPrice: 3000,
+    },
+    {
+      cName: "FrontEnd Development",
+      cPrice: 2000,
+    },
+    {
+      cName: "Manual QA",
+      cPrice: 1000,
+    },
+  ];
+
   changeName(newName) {
     if (newName.localCompare(this.sName) === 0) {
-      console.log(
-        `Name cannot be updated; as you have same name already stored in the system.${this.sName}`);
+      console.log(`\nName cannot be updated; as you have same name already stored in the system.${this.sName}`);
     } else {
       this.sName = newName;
-      console.log(`You've succesfully updated your name ${this.sName}`);
+      console.log(`\nYou've succesfully updated your name to ${this.sName}.`);
     }
   }
 
   changeCourse(newCourse) {
-    if (newCourse.localCompare(this.cName) === 0) {
-      console.log(
-        `Course cannot be updated as you have same course Registered already. ${this.cName}`);
+    if (newCourse.localCompare(this.sCourseName) === 0) {
+      console.log(`\nCourse cannot be updated as you have same course Registered already, ${this.sCourseName}.`);
     } else if (newCourse !== this.cName) {
-      console.log(`We do not have this course ${this.cName}`);
+      console.log(`\nWe do not have this course, ${newCourse}.`);
     } else {
-      this.cName = newCourse;
-      console.log(`You've succesfully updated your course. ${this.cName}`);
+      this.sCourseName = newCourse;
+      console.log(`\nYou've succesfully updated your course to ${this.sCourseName}`);
     }
   }
 
   makePayment(amount) {
     if (amount <= 0) {
-      console.log(`Plase enter correct amount`);
-    } else if (amount > this.cName.balance) {
-      console.log(`you cannot pay more than the balance amount ${amount}`);
+      console.log(`\nPlase enter correct amount`);
+    } else if (amount > this.sBalance) {
+      console.log(`\nYou cannot pay more than the Course price amount, ${amount}`);
     } else {
-      amount -= this.cName.balance;
-      console.log(
-        `\nThank you for the payment of $${amount}. Your updated balance is $${this.cName.balance}`);
+      amount -= this.sBalance;
+      console.log(`\nThank you for the payment of $${amount}. Your updated balance is $${this.sBalance}`);
     }
   }
 }
