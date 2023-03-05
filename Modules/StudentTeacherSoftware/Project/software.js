62aa340dafff5ea43e364996f946cf00caf2b2ff
Jeffrey Rodriguez <jeffreyrodriguez@Jeffreys-MacBook-Pro.local>
2023-03-05 07:51:07 -0500

    Parent File

---
+++ Modules/StudentTeacherSoftware/Project/software.js
@@ -0,0 +1,9 @@
+const Restaurant = require("./Restaurant");
+const House = require("./House");
+
+
+const r1 = new Restaurant("R1", "c2", 10, 2);
+r1.showRestaurantDetails();
+
+const h1 = new House("Mark","New York Queens", 3);
+h1.showHouseDetails();
