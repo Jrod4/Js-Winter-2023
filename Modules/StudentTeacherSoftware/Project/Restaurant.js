eafd401718b203019cf486075ab510c75fe6aa50
Jeffrey Rodriguez <jeffreyrodriguez@Jeffreys-MacBook-Pro.local>
2023-03-07 21:16:23 -0500

    Restaurant class file updated

--- Modules/StudentTeacherSoftware/Project/Restaurant.js
+++ Modules/StudentTeacherSoftware/Project/Restaurant.js
@@ -1,19 +1,26 @@
+// const Members = require("./Members");
+
+// class Restaurant extends Members {
+
+
+// }
+
 class Restaurant {
   static RestaurantIdCounter = 0;
 
   restaurantInfo = {
     rName: "",
     rCity: "",
-    rIsOpen: false, //currentStatus
+    CurrentStatus: "closed",
     rGuestsSittingNow: 0,
     rMaxGuestCapacity: 0,
     rTotalEmployees: 0,
-    restaurantId: 0
+    restaurantId: 0,
   };
 
   constructor(restaurantName, cityName, maxGuestCapacity, totalEmployees) {
-    if (restaurantName.length < 1 ) {
-      console.log('invalid input requires more than one letter');
+    if (restaurantName.length < 1) {
+      console.log("invalid input requires more than one letter");
     } else if (cityName.length < 1) {
       console.log("invalid input requires more than one letter");
     } else if (maxGuestCapacity < 1) {
@@ -21,44 +28,44 @@ class Restaurant {
     } else if (totalEmployees < 1) {
       console.log("invalid input requires more than one letter");
     } else {
-    this.restaurantInfo.rName = restaurantName;
-    this.restaurantInfo.rCity = cityName;
-    this.restaurantInfo.rMaxGuestCapacity = maxGuestCapacity;
-    this.restaurantInfo.rTotalEmployees = totalEmployees;
-    this.restaurantInfo.restaurantId = ++Restaurant.RestaurantIdCounter;
-    console.log(`\nThank you for registering! ${this.restaurantInfo.rName}`);
-    console.log(`\nThis is your restaurant Id is ${this.restaurantInfo.restaurantId}`);
+      this.restaurantInfo.rName = restaurantName;
+      this.restaurantInfo.rCity = cityName;
+      this.restaurantInfo.rMaxGuestCapacity = maxGuestCapacity;
+      this.restaurantInfo.rTotalEmployees = totalEmployees;
+      this.restaurantInfo.restaurantId = ++Restaurant.RestaurantIdCounter;
+      console.log(`\nThank you for registering! ${this.restaurantInfo.rName}`);
+      console.log(
+        `\nThis is your restaurant Id is ${this.restaurantInfo.restaurantId}`
+      );
     }
   }
 
   open() {
-         if (rIsOpen === true){
-          console.log('Restaurant is already open');
-        } else {
-          rIsOpen = true;
-          console.log('the Restaurant is open');
-        }
-}
+    if (this.restaurantInfo.CurrentStatus === "open") {
+      console.log("Restaurant is already open");
+    } else {
+      this.restaurantInfo.CurrentStatus = "open";
+      console.log("the Restaurant is open");
+    }
+  }
 
-
-
-  close() { 
-      if (rIsOpen === false) {
-        console.log("Restaurant is already closed");
-      } else {
-        rIsOpen = false;
-        console.log("the Restaurant is closed");
-      }
-
+  close() {
+    if (this.restaurantInfo.CurrentStatus === "closed") {
+      console.log("Restaurant is already closed");
+    } else {
+      this.restaurantInfo.CurrentStatus = "closed";
+      console.log("the Restaurant is closed");
+    }
   }
 
   showRestaurantDetails() {
-    console.log(`\nRestaurant Details`);
+    console.log(`\nRestaurant Details\n`);
     console.log(`Name : ${this.restaurantInfo.rName}`);
     console.log(`City : ${this.restaurantInfo.rCity}`);
     console.log(`Max Guest : ${this.restaurantInfo.rMaxGuestCapacity}`);
     console.log(`Total Employees: ${this.restaurantInfo.rTotalEmployees}`);
     console.log(`Restaurant Id : ${this.restaurantInfo.restaurantId}`);
+    console.log(`Current Status : ${this.restaurantInfo.CurrentStatus}`);
   }
 }
 module.exports = Restaurant;
