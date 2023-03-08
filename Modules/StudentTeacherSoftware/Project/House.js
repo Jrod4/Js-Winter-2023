0fac4db40e7621aaa0169a8c9a3da2bc0535048f
Jeffrey Rodriguez <jeffreyrodriguez@Jeffreys-MacBook-Pro.local>
2023-03-07 21:14:50 -0500

    House class file updated

--- Modules/StudentTeacherSoftware/Project/House.js
+++ Modules/StudentTeacherSoftware/Project/House.js
@@ -1,49 +1,52 @@
+// const Members = require("./Members");
+
+// class house extends Members {}
+
 class House {
   static houseIdCounter = 0;
 
   houseInfo = {
-    Address: "-",
+    Name: "-",
     HouseId: 0,
     City: "-",
-    CurrentStatus: "-",
+    CurrentStatus: 'locked',
     TotalMembers: 0,
   };
 
-  constructor(address, cityName, houseMembers) {
+  constructor(Name, cityName, houseMembers) {
     // Create if, else if
 
     this.houseInfo.City = cityName;
     this.houseInfo.TotalMembers = houseMembers;
-    this.houseInfo.Address = address;
+    this.houseInfo.Name = Name;
     this.houseInfo.CurrentStatus = this.houseInfo.CurrentStatus;
     this.houseInfo.HouseId = ++House.houseIdCounter;
-    console.log(
-      `Your allowed to use, current status is ${this.houseInfo.CurrentStatus}!`
-    );
-    console.log(`Your new house Id is ${this.houseInfo.HouseId}!`);
+    // console.log(`\nYour allowed to use, current status is ${this.houseInfo.CurrentStatus}!`
+    // );
+    console.log(`\nYour new house Id is ${this.houseInfo.HouseId}`);
   }
 
   lock() {
-    if (CurrentStatus === true) {
+    if (this.houseInfo.CurrentStatus === 'locked') {
       console.log("House is already locked");
-    } else {
-      CurrentStatus = true;
-      console.log("House is locked");
+    } else if (this.houseInfo.CurrentStatus === 'unlocked') {
+      this.houseInfo.CurrentStatus = 'locked';
+      console.log("House is now locked");
     }
   }
 
   unlock() {
-    if (CurrentStatus === false) {
+    if (this.houseInfo.CurrentStatus === 'unlocked') {
       console.log("House is already unlocked");
-    } else {
-      CurrentStatus = false;
+    } else if (this.houseInfo.CurrentStatus === 'locked') {
+      this.houseInfo.CurrentStatus = 'unlocked';
       console.log("The house is unlocked");
-    }
+    } 
 
   }
 
   showHouseDetails() {
-    console.log(`\nHouse Details`);
+    console.log(`House Details\n`);
     console.log(`Name : ${this.houseInfo.Name}`);
     console.log(`City : ${this.houseInfo.City}`);
     console.log(`Current Status : ${this.houseInfo.CurrentStatus}`);
