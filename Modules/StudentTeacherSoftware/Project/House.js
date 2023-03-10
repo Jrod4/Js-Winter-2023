69883f2c857ea085bbd65f298b6c6284f1847964
Jeffrey Rodriguez <jeffreyrodriguez@Jeffreys-MacBook-Pro.local>
2023-03-09 20:03:08 -0500

    House Class File 2

--- Modules/StudentTeacherSoftware/Project/House.js
+++ Modules/StudentTeacherSoftware/Project/House.js
@@ -1,48 +1,68 @@
-// const Members = require("./Members");
+const Members = require("./Members");
 
-// class house extends Members {}
+// class House extends Members {
+//   clean() {
+//     console.log('House area is now clean');
+//     super.clean();
+//   }
+//   }
 
-class House {
+class House extends Members {
   static houseIdCounter = 0;
 
   houseInfo = {
     Name: "-",
     HouseId: 0,
     City: "-",
-    CurrentStatus: 'locked',
-    TotalMembers: 0,
+    CurrentStatus: "locked",
+    houseMembers: 0,
   };
 
   constructor(Name, cityName, houseMembers) {
-    // Create if, else if
-
-    this.houseInfo.City = cityName;
-    this.houseInfo.TotalMembers = houseMembers;
-    this.houseInfo.Name = Name;
-    this.houseInfo.CurrentStatus = this.houseInfo.CurrentStatus;
-    this.houseInfo.HouseId = ++House.houseIdCounter;
-    // console.log(`\nYour allowed to use, current status is ${this.houseInfo.CurrentStatus}!`
-    // );
-    console.log(`\nYour new house Id is ${this.houseInfo.HouseId}`);
+    super();
+    if (this.houseInfo.Name === "") {
+      console.log(`\nPlease enter a name.`);
+    } else if (this.houseInfo.cityName === "") {
+      console.log(`\nPlease enter your city`);
+    } else if (this.houseInfo.houseMembers === "") {
+      console.log(`\nPlease enter the number of members in household.`);
+    } else {
+      this.houseInfo.City = cityName;
+      this.membersInfo.TotalMembers = houseMembers;
+      this.houseInfo.Name = Name;
+      this.houseInfo.CurrentStatus = this.houseInfo.CurrentStatus;
+      this.houseInfo.HouseId = ++House.houseIdCounter;
+      // console.log(`\nYour allowed to use, current status is ${this.houseInfo.CurrentStatus}!`
+      // );
+      console.log(`\nYour new house Id is ${this.houseInfo.HouseId}`);
+    }
   }
 
   lock() {
-    if (this.houseInfo.CurrentStatus === 'locked') {
+    if (this.houseInfo.CurrentStatus === "locked") {
       console.log("House is already locked");
-    } else if (this.houseInfo.CurrentStatus === 'unlocked') {
-      this.houseInfo.CurrentStatus = 'locked';
+    } else if (this.houseInfo.CurrentStatus === "unlocked") {
+      this.houseInfo.CurrentStatus = "locked";
       console.log("House is now locked");
     }
   }
 
   unlock() {
-    if (this.houseInfo.CurrentStatus === 'unlocked') {
+    if (this.houseInfo.CurrentStatus === "unlocked") {
       console.log("House is already unlocked");
-    } else if (this.houseInfo.CurrentStatus === 'locked') {
-      this.houseInfo.CurrentStatus = 'unlocked';
+    } else if (this.houseInfo.CurrentStatus === "locked") {
+      this.houseInfo.CurrentStatus = "unlocked";
       console.log("The house is unlocked");
-    } 
+    }
+  }
 
+  clean() {
+    console.log("House area is now clean");
+    super.clean();
+  }
+
+  showTotalMembers() {
+    console.log(`Total Members: ${this.houseInfo.houseMembers}`);
   }
 
   showHouseDetails() {
@@ -50,7 +70,15 @@ class House {
     console.log(`Name : ${this.houseInfo.Name}`);
     console.log(`City : ${this.houseInfo.City}`);
     console.log(`Current Status : ${this.houseInfo.CurrentStatus}`);
-    console.log(`Total Members: ${this.houseInfo.TotalMembers}`);
+    console.log(`Total House Members: ${this.membersInfo.TotalMembers}`);
     console.log(`House Id : ${this.houseInfo.HouseId}`);
   }
-}module.exports = House;
+  addHouseMembers(numMemToAdd) {
+    this.addMembers(numMemToAdd, 'member');
+  }
+
+  removeHouseMember(numMemToRemove) {
+    this.removeMembers(numMemToRemove, "member");
+  }
+}
+module.exports = House;
