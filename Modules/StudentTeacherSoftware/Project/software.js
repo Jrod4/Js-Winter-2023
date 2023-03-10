b2a0133f62c12e87e1ad4eabadc8df9ca79efe5e
Jeffrey Rodriguez <jeffreyrodriguez@Jeffreys-MacBook-Pro.local>
2023-03-09 21:20:57 -0500

    Software

--- Modules/StudentTeacherSoftware/Project/software.js
+++ Modules/StudentTeacherSoftware/Project/software.js
@@ -2,22 +2,25 @@ const Restaurant = require("./Restaurant");
 const House = require("./House");
 const Members = require("./Members");
 
+const r1 = new Restaurant("R1", "c2", 50, 20);
+r1.close();
+r1.open();
+r1.showRestaurantDetails();
+r1.close();
+r1.showRestaurantDetails();
+r1.removeEmployee(1);
+r1.showRestaurantDetails();
+r1.guestSittingNow(10);
+r1.showCurrentGuestCount();
+r1.guestSittingNow(-1);
 
+// const h2 = new House("Mark", "New York Queens", 1000);
 
-const r1 = new Restaurant("R1", "c2", 10, 2);
-r1.close()
-r1.open()
-r1.showRestaurantDetails(`\n`);
-
-const h2 = new House("Mark","New York Queens", 3);
- 
- h2.unlock()
- //h2.lock();
- h2.showHouseDetails();
-
-const m3 = new Members("jeff", "boston", 8);
-m3.addMembers(2)
-
-
-
-
+// h2.unlock()
+// h2.lock();
+// h2.clean()
+// h2.showHouseDetails();
+// h2.showTotalMembers();
+// h2.addHouseMembers(100);
+// h2.removeHouseMember(20);
+// h2.showHouseDetails();
