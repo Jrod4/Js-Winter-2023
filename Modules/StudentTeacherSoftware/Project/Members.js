a80ead81af6e721d90bbaff1f9b185b09ba7d9ec
Jeffrey Rodriguez <jeffreyrodriguez@Jeffreys-MacBook-Pro.local>
2023-03-09 20:05:37 -0500

    Members Parent File

--- Modules/StudentTeacherSoftware/Project/Members.js
+++ Modules/StudentTeacherSoftware/Project/Members.js
@@ -1,50 +1,62 @@
 class Members {
-  
-membersInfo = {
+  membersInfo = {
     Name: "-",
-    HouseId: 0,
     City: "-",
     CurrentStatus: true,
     TotalMembers: 0,
   };
 
-  // constructor() {
-  // this.isClean = false;
-  // }
-
   clean() {
-    this.isClean = true;
-    console.log("The restaurant is cleaned now");
+    console.log("Area is clean now");
   }
 
   addMembers(x) {
     if (x < 1) {
-      console.log("Please input atleast 1 member!");
-    } else if (isNaN(x)) {
-      console.log("Please input a number!");
+      console.log(`Please input atleast 1 ${memberType}!`);
+    } else if (x > this.membersInfo.TotalMembers) {
+      console.log(
+        `There are less ${memberType}s that the number of ${memberType}s you want to add!`
+      );
+    } else if (typeof x !== "number") {
+      console.log("Please make sure to input a NUMBER");
     } else {
-      this.membersInfo.TotalMembers += x;
+      this.membersInfo.TotalMembers -= x;
+      console.log(`\nSuccessfully added ${y} ${memberType}`);
+      console.log(`Total ${memberType}: ${this.membersInfo.TotalMembers}`);
     }
   }
 
-  removeMembers(y) {
+  // removeMembers(y) {
+  //   let actionResult = "";
+  //   if (y < 1) {
+  //     actionResult = "Error1";
+  //   } else if (y > this.membersInfo.TotalMembers) {
+  //     actionResult = "Error2";
+  //   } else if (typeof y !== "number") {
+  //     actionResult = "Error3";
+  //   } else {
+  //     this.membersInfo.TotalMembers -= y;
+  //     actionResult = "Success";
+  //   }
+  //   return actionResult;
+  // }
+
+  removeMembers(y, memberType) {
     if (y < 1) {
-      console.log("Please input atleast 1 member!");
-    } else if (isNaN(y)) {
-      console.log("Please input a number!");
-    } else if (y > TotalMembers) {
+      console.log(`Please input atleast 1 ${memberType}!`);
+    } else if (y > this.membersInfo.TotalMembers) {
       console.log(
-        "There are less members that the number of members you want to remove!"
+        `There are less ${memberType}s that the number of ${memberType}s you want to remove!`
       );
+    } else if (typeof y !== "number") {
+      console.log("Please make sure to input a NUMBER");
     } else {
       this.membersInfo.TotalMembers -= y;
+      console.log(`\nSuccessfully removed ${y} ${memberType}`);
+      console.log(`Total ${memberType}: ${this.membersInfo.TotalMembers}`);
     }
   }
 
-  showCurrentMembers() {
-
+  showCurrentMembers() {}
 }
-
-
-}module.exports = Members;
-
+module.exports = Members;
